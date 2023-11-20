c47e54d442d71ca77d0b12fc633c9dba7191a823
marsen <admin@marsen.me>
2023-11-20 16:02:43 +0800

    Update deployment script

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,18 +19,19 @@ npm install hexo --save
 npm install hexo-util --save
 
 # deployment
-if [ "$INPUT_COMMIT_MSG" = "none" ]
-then
-    hexo g -d
-elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
-then
-    # pull original publish repo
-    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /usr/app/sync_deploy_history.js
-    hexo g -d
-else
-    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /usr/app/sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
-fi
+#if [ "$INPUT_COMMIT_MSG" = "none" ]
+#then
+#    hexo g -d
+#elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
+#then
+#    # pull original publish repo
+#    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /usr/app/sync_deploy_history.js
+#    hexo g -d
+#else
+#    NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /usr/app/sync_deploy_history.js
+#    hexo g -d -m "$INPUT_COMMIT_MSG"
+#fi
+
 
 result="Deploy complate."
 
